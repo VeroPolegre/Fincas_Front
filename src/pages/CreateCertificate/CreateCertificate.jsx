b39dc97da6d9ac6429d988f5deda9973ef208e5a
VeroPolegre <veronicapolegre98@gmail.com>
2024-01-12 14:00:31 +0100

    createCertificate page

---
+++ src/pages/CreateCertificate/CreateCertificate.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import Header from "../../components/Header/Header";
+import { uploadImage } from "../../features/certificates/certificateSlice";
+
+export default function CreateCertificate() {
+  const dispatch = useDispatch();
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUploadImage = () => {
+    dispatch(uploadImage(file));
+  };
+
+  return (
+    <>
+      <Header />
+      <h1>Actas</h1>
+      <form>
+        <label>
+          Adjuntar archivo:
+          <input type="file" onChange={handleFileChange} />
+        </label>
+        <br />
+        <button type="button" onClick={handleUploadImage}>
+          Resumir acta
+        </button>
+      </form>
+    </>
+  );
+}
