ffe2fb9f35eff208617fb132ef62bb0ffc1f62e2
Francesc Alberola <f.alberola@gmail.com>
2024-01-13 20:48:36 +0100

    first steps profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Header from "../../components/Header/Header";
+import iconoCasa from "../../assets/iconoCasa.png";
+import iconoLupa from "../../assets/iconoLupa.png";
+import iconoCampana from "../../assets/iconoCampana.png";
+import iconoUsuario from "../../assets/iconoUsuario.png";
+import "./Profile.scss";
 
 const Profile = () => {
   return (
     <>
-      <Header />
+      <img className="iconoCasa" src={iconoCasa} alt="iconoCasa" />
+      <div className="icons-container">
+        <img className="iconoLupa" src={iconoLupa} alt="iconoLupa" />
+        <img className="iconoCampana" src={iconoCampana} alt="iconoCampana" />
+        <img className="iconoUsuario" src={iconoUsuario} alt="iconoUsuario" />
+      </div>
+
       <h1>Profile</h1>
     </>
   );
