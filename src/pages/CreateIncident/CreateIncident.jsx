fa72c42508acd21b233485cd67973eca7d531856
VeroPolegre <veronicapolegre98@gmail.com>
2024-01-14 10:40:55 +0100

    scss tree containers, removing padding and adding containers and width instead

--- src/pages/CreateIncident/CreateIncident.jsx
+++ src/pages/CreateIncident/CreateIncident.jsx
@@ -14,28 +14,46 @@ const CreateIncident = () => {
       <h1 className="incident-title">
         Crear <br /> incidencia
       </h1>
-      <div className="camera-button">
-        <img className="icono-camara" src={iconoCamara} alt="iconoCamara" />
-        <span className="button-title">Cámara</span>
-      </div>
-      <div className="gallery-button">
-        <img className="icono-gallery" src={iconoGallery} alt="iconoGalería" />
-        <span className="button-title">Galería</span>
-      </div>
-      <div className="text-button">
-        <img className="icono-text" src={iconoText} alt="iconoTexto" />
-        <span className="button-title">Texto</span>
-      </div>
-      <div className="audio-button">
-        <img className="icono-audio" src={iconoAudio} alt="iconoAudio" />
-        <span className="button-title">Audio</span>
-      </div>
-      <div className="call-button">
-        <img className="icono-call" src={iconoCall} alt="iconoCall" />
-        <span className="button-title">Llamada</span>
-      </div>
-      <div className="cancel-button">
-        <span className="button-title">Cancelar</span>
+      <div className="buttons-container">
+        <div className="camera-button ">
+          <div className="icon-container">
+            <img className="icono-camara" src={iconoCamara} alt="iconoCamara" />
+            <span className="button-title">Cámara</span>
+          </div>
+        </div>
+        <div className="gallery-button">
+          <div className="icon-container">
+            <img
+              className="icono-gallery"
+              src={iconoGallery}
+              alt="iconoGalería"
+            />
+            <span className="button-title">Galería</span>
+          </div>
+        </div>
+        <div className="text-button">
+          <div className="icon-container">
+            <img className="icono-text" src={iconoText} alt="iconoTexto" />
+            <span className="button-title">Texto</span>
+          </div>
+        </div>
+        <div className="audio-button">
+          <div className="icon-container">
+            <div className="icon-container">
+              <img className="icono-audio" src={iconoAudio} alt="iconoAudio" />
+              <span className="button-title">Audio</span>
+            </div>
+          </div>
+        </div>
+        <div className="call-button">
+          <div className="icon-container">
+            <img className="icono-call" src={iconoCall} alt="iconoCall" />
+            <span className="button-title">Llamada</span>
+          </div>
+        </div>
+        <div className="cancel-button">
+          <span className="button-title">Cancelar</span>
+        </div>
       </div>
     </>
   );
