e5bb3d12f5d5ea37662487a5c889539503338854
VeroPolegre <veronicapolegre98@gmail.com>
2024-01-16 00:31:09 +0100

    home admin complete scss and create certificate navigate

--- src/pages/HomeAdmin/HomeAdmin.jsx
+++ src/pages/HomeAdmin/HomeAdmin.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import "./HomeAdmin.scss";
-import iconoIncidencia from "../../assets/iconoIncidencia.png";
-import iconoComunidad from "../../assets/iconoComunidad.png";
-
+import iconoCalendario from "../../assets/iconoCalendario.png";
+import iconoRecibo from "../../assets/iconoRecibo.png";
+import iconoArea from "../../assets/iconoArea.png";
+import { useNavigate } from "react-router-dom";
 const HomeAdmin = () => {
+  const navigate = useNavigate();
+  const handleCreateIncidentClick = () => {
+    navigate("/create-certificate");
+  };
   return (
     <>
       <Header />
@@ -23,58 +28,66 @@ const HomeAdmin = () => {
             </div>
           </div>
         </div>
-        <div className="create-incidence-button-admin">
+        <div
+          className="create-incidence-button-admin"
+          onClick={handleCreateIncidentClick}
+        >
           <div className="icon-container-admin">
             <h5 className="home-create-incident-admin">Crear acta</h5>
             {/* <span className="home-create-incident-subtitle-admin">acta</span> */}
           </div>
         </div>
-        <div className="comunity-button-admin">
-          <div className="icon-container-admin">
-            <img
-              className="icono-comunity-admin"
-              src={iconoComunidad}
-              alt="iconoComunidad"
-            />
-            <span className="button-comunity-title-white-admin">
-              Mi Comunidad
-            </span>
+        <div style={{ display: "flex" }}>
+          <div className="comunity-button-admin">
+            <div className="icon-container-admin">
+              <span className="button-comunity-white-admin">Cuba, 64</span>
+              <span className="button-comunity-title-white-admin">13:00</span>
+            </div>
           </div>
-        </div>
-
-        <div className="junta-button-admin">
-          <div className="icon-container-admin">
-            <span className="junta-title-white-admin">Junta</span>
-            <span className="button-title-white-admin custom-size-junta-admin">
-              21
-            </span>
-            <span className="enero-title-white-admin">Enero</span>
+          <div className="junta-button-admin">
+            <div className="icon-container-admin">
+              <span className="button-comunity-black-admin">Alboraya, 27</span>
+              <span className="button-comunity-title-black-admin">13:00</span>
+            </div>
+          </div>
+          <div className="calendario-admin">
+            <div className="icon-container-admin">
+              <span className="button-calendario-black-admin">Calendario</span>
+              <img
+                className="icono-calendario"
+                src={iconoCalendario}
+                alt="iconoCalendario"
+              />
+            </div>
           </div>
         </div>
         <div className="cuota-button-admin">
           <div className="icon-container-admin">
-            <span className="button-title-admin">Cuota</span>
-            <span className="button-title-admin custom-size-cuota-admin">
-              530
-            </span>
-            <span className="button-title-admin">€/año</span>
+            <img className="icono-recibo" src={iconoRecibo} alt="iconoRecibo" />
+            <span className="pendiente-title-admin">Recibos pendientes</span>
           </div>
         </div>
-        <div className="reservar-button-admin">
+        <div className="area-button-admin">
           <div className="icon-container-admin">
-            <span className="button-title-admin">Reservar</span>
-            <span className="button-title-admin">instalaciones</span>
+            <span className="area-title-admin">Área privada</span>
+            <img className="icono-area" src={iconoArea} alt="iconoArea" />
           </div>
         </div>
-        <div className="votacion-button-admin">
-          <div className="icon-container-admin">
-            <div className="icon-container-admin">
-              <span className="button-title-admin">Votación</span>
-              <span className="button-title-admin custom-size-votacion-admin">
-                01
-              </span>
-              <span className="button-title-admin">pendiente</span>
-            </div>
+        <div className="incidencias-button-admin">
+          <div className="icon-container-admin" style={{ color: "white" }}>
+            <span className="incidencias-title-admin">Incidencias</span>
+            <h2
+              style={{
+                fontSize: "50px",
+                marginTop: "80px",
+                marginLeft: "-80px",
+                marginRight: "50px",
+                fontWeight: 500,
+                color: "white",
+              }}
+            >
+              7
+            </h2>
           </div>
         </div>
       </div>
