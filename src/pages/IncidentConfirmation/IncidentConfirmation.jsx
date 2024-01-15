e3d9f58e4520e47223962a3cc718fcd8d37c488f
Francesc Alberola <f.alberola@gmail.com>
2024-01-15 20:52:26 +0100

    incident-confirmation scss

--- src/pages/IncidentConfirmation/IncidentConfirmation.jsx
+++ src/pages/IncidentConfirmation/IncidentConfirmation.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import { useNavigate } from "react-router-dom";
+import checkmark from "../../assets/checkmark.png";
+import iconoBombilla from "../../assets/IconoBombilla.png";
+import "./IncidentConfirmation.scss";
 
 const IncidentConfirmation = () => {
   const navigate = useNavigate();
@@ -10,20 +13,37 @@ const IncidentConfirmation = () => {
   return (
     <>
       <Header />
-      <h1>Gracias por tu aviso</h1>
-      <h3>Pronto estará arreglado</h3>
-      <div>
-        <p>Se ha registrado tu incidencia</p>
-      </div>
-      <div>
-        <h5>Incidencia #1</h5>
-        <p>Rotura de bombilla</p>
-        <p>19/01/2024 - Ref: 6455749846</p>
-      </div>
-      <div className="next-button" onClick={handleContinuarClick}>
-        <span className="button-title" style={{ color: "white" }}>
-          Continuar
-        </span>
+      <div className="incident-confirmation-container">
+        <h1 className="incident-confirmation-title">Gracias por tu aviso</h1>
+        <h3 className="incident-confirmation-subtitle">
+          Pronto estará arreglado
+        </h3>
+        <div className="image-confirmation-container">
+          <h3 className="confirmed-incident-text">
+            Se ha registrado
+          </h3>
+          <h3 className="confirmed-incident-text-two">
+          tu incidencia
+          </h3>
+          <img src={checkmark} alt="checkmark" className="checkmark-image" />
+        </div>
+        <div className="floor-confirmation-container">
+          <h2 className="incidence-confirmation-text">Incidencia #1</h2>
+          <h5 className="incidence-subtitle-text">Rotura de bombilla</h5>
+          <h5 className="incidence-date-text">19/01/2024 - Ref: 6455749846 </h5>
+          <p>
+            <img
+              src={iconoBombilla}
+              alt="iconoBombilla"
+              className="iconoBombilla"
+            />
+          </p>
+        </div>
+        <div className="next-button" onClick={handleContinuarClick}>
+          <span className="button-title" style={{ color: "white" }}>
+            Ver seguimiento
+          </span>
+        </div>
       </div>
     </>
   );
