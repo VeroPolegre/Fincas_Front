fbdab14f0b42554d53abe1c51b4eb20f7e82b93b
Francesc Alberola <f.alberola@gmail.com>
2024-01-16 13:42:58 +0100

    search-comunity scss

---
+++ src/pages/SearchComunity/SearchComunity.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import iconoWallet from "../../assets/walletSearchComunity.png";
+import iconoSoloLupa from "../../assets/iconoSoloLupa.png"
+import iconoSoloMicro from "../../assets/iconoSoloMicro.png"
+import "./SearchComunity.scss";
+
+const SearchComunity = () => {
+  const [formData, setFormData] = useState({});
+
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <Header />
+      
+      <div className="create-certificate-search-container">
+        <h1 className="search-confirmation-title">Crear acta</h1>
+        <h3 className="search-confirmation-subtitle">indica la comunidad</h3>
+
+        <form className="form-register">
+          <div className="custom-label-input">
+            <div className="input-with-icon">
+            <img
+                src={iconoSoloLupa}
+                alt="iconoSoloLupa"
+                className="iconoSoloLupa"
+              />
+            <img
+                src={iconoSoloMicro}
+                alt="iconoSoloMicro"
+                className="iconoSoloMicro"
+              />
+              <input
+                className="input-search-comunity"
+                type="text"
+                name="text"
+                placeholder="Buscar comunidad"
+                value={formData.text}
+                onChange={onChange}
+              />
+              
+            </div>
+          </div>
+        </form>
+        <div>
+          <img src={iconoWallet} alt="iconoWallet" className="iconoWallet" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchComunity;
