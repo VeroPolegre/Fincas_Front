842abbb57e24d6abff548dcfcc6db4191cbddcf2
VeroPolegre <veronicapolegre98@gmail.com>
2024-01-16 20:32:42 +0100

    certificate details complete and resume scss

--- src/pages/CertificateDetails/CertificateDetails.jsx
+++ src/pages/CertificateDetails/CertificateDetails.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import atrasIcono from "../../assets/atrasIcono.png";
+import iconoArchivo from "../../assets/iconoArchivo.png";
+import iconoAltavoz from "../../assets/iconoAltavoz.png";
+import iconoEdit from "../../assets/iconoEdit.png";
 import "./CertificateDetails.scss";
 import { useNavigate } from "react-router-dom";
 
@@ -10,6 +13,10 @@ const CertificateDetails = () => {
   const navigate = useNavigate();
   const handleAtrasIconoClick = () => {
     navigate("/create-certificate");
+  };
+
+  const handleContinuarClick = () => {
+    navigate("/incident-tracking");
   };
   // const [isModalOpen, setIsModalOpen] = useState(false);
   // const showModal = () => {
@@ -36,13 +43,39 @@ const CertificateDetails = () => {
           C. de Cuba, 64, Valencia
         </span>
       </div>
-      <div className="certificate-details-buttons-container">
-        <button className="certificate-camera-button">
-          <div className="icon-container">
-            <span className="button-title">Cámara</span>
-          </div>
-        </button>
+      <div className="certificate-details-file-container">
+        <img src={iconoEdit} alt="iconoEdit" className="iconoEdit" />
       </div>
+      <div className="complete-certificate-button">
+        <div className="icon-container">
+          <h5 className="complete-certificate-title">Ver acta completa</h5>
+          <img src={iconoArchivo} alt="iconoArchivo" className="iconoArchivo" />
+        </div>
+      </div>
+      <div className="listen-certificate-button">
+        <div className="icon-container">
+          <h5 className="listen-certificate-title">Escuchar acta</h5>
+          <img src={iconoAltavoz} alt="iconoAltavoz" className="iconoAltavoz" />
+        </div>
+      </div>
+      <div className="resume-button">
+        <div className="icon-container">
+          <h5 className="resume-title">Ver acta resumida</h5>
+          <img src={iconoArchivo} alt="iconoArchivo" className="iconoArchivo" />
+        </div>
+      </div>
+      <div className="listen-resume-button">
+        <div className="icon-container">
+          <h5 className="listen-resume-title">Escuchar resumen</h5>
+          <img src={iconoAltavoz} alt="iconoAltavoz" className="iconoAltavoz" />
+        </div>
+      </div>
+      <div className="next-button" onClick={handleContinuarClick}>
+        <span className="button-title" style={{ color: "white" }}>
+          Crear acta
+        </span>
+      </div>
+
       {/* 
       <Button type="secundary" onClick={showModal}>
         Open Modal
