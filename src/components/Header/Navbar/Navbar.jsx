f6ed62670ee591086ecb7326bb7fea25ba218308
Francesc Alberola <f.alberola@gmail.com>
2024-01-17 11:06:33 +0100

    change iconoLupa & iconoInfo NavBar

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import iconoCasa from "../../../assets/iconoCasa.png";
 import iconoLupa from "../../../assets/iconoLupa.png";
+import iconoInfo from "../../../assets/iconoInfo.png";
 import iconoCampana from "../../../assets/iconoCampana.png";
 import iconoUsuario from "../../../assets/iconoUsuario.png";
 import iconoAdmin from "../../../assets/iconoAdmin.png";
@@ -11,25 +12,10 @@ import { Dropdown, Space } from "antd";
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.auth);
-  const items = [
-    {
-      key: "1",
-      label: (
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.thebridge.tech/"
-        >
-          The Bridge
-        </a>
-      ),
-    },
-    {
-      key: "2",
-      danger: true,
-      label: "Cerrar sesion",
-    },
-  ];
+  const currentPath = window.location.pathname;
+
+  const showLupa = currentPath === "/home-admin";
+
   return (
     <>
       {user && (
@@ -39,16 +25,42 @@ const Navbar = () => {
       )}
 
       <div className="icons-container" style={{ marginLeft: 147 }}>
-        <img className="iconoLupa" src={iconoLupa} alt="iconoLupa" />
+        {(showLupa ? (
+          <img className="iconoLupa" src={iconoLupa} alt="iconoLupa" />
+        ) : (
+          <img className="iconoLupa" src={iconoInfo} alt="iconoInfo" />
+        ))}
         <img className="iconoCampana" src={iconoCampana} alt="iconoCampana" />
         <Dropdown
           menu={{
-            items,
+            items: [
+              {
+                key: "1",
+                label: (
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.thebridge.tech/"
+                  >
+                    The Bridge
+                  </a>
+                ),
+              },
+              {
+                key: "2",
+                danger: true,
+                label: "Cerrar sesion",
+              },
+            ],
           }}
         >
           <a onClick={(e) => e.preventDefault()}>
             {user && user.role === "admin" ? (
-              <img className="iconoUsuario" src={iconoAdmin} alt="iconoAdmin" />
+              <img
+                className="iconoUsuario"
+                src={iconoAdmin}
+                alt="iconoAdmin"
+              />
             ) : (
               <img
                 className="iconoUsuario"
